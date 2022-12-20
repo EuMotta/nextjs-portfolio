31227e18f74fede561c4a5208a4a36c81e3fc389
EuMotta <hyperxmotta@hotmail.com>
2022-12-20 09:53:55 -0300

    Experience2 update 1

--- sections/Experience2.jsx
+++ sections/Experience2.jsx
@@ -24,51 +24,110 @@ const Experience = () => (
           variants={fadeIn('right', 'tween', 0, 1.5)}
           className="grid grid-cols-2 "
         >
-          <div className="col-span-1 ml-20 bg-slate-800 p-5 rounded-xl shadow-lg shadow-slate-900  text-white flex justify-center">
-            <div className="w-full   border-l-2 flex flex-col border-orange-600 z-10">
-              <div>
-                <span>
-                  <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
-                </span>
-                <div className="px-5 flex flex-col gap-y-3">
-                  <div className=" flex items-center gap-x-2">
-                    <AiFillCalendar className="text-xl" /> 2022 - 2025
+          <div className="col-span-1 ml-20   text-white flex flex-col justify-center">
+            <TypingText
+              title="Cursos e formações"
+              textStyles="text-center text-secondary-white font-bold mb-5 text-3xl"
+            />
+            <div className="bg-slate-800 p-5 rounded-xl shadow-lg shadow-slate-900">
+              <div className="w-full   border-l-2 flex flex-col border-orange-600 z-10">
+                <div>
+                  <span>
+                    <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
+                  </span>
+                  <div className="px-5 flex flex-col gap-y-3">
+                    <div className=" flex items-center gap-x-2">
+                      <AiFillCalendar className="text-xl" /> 2022 - 2025
+                    </div>
+                    <div className="text-secondary-white text-xl">
+                      Tecnologo pela FATEC Guaratinguetá
+                    </div>
+                    <p>
+                      Formei-me em Análise e Desenvolvimento de Sistemas pela
+                      FATEC Guaratinguetá, o que marcou o início de minha
+                      carreira como programador. Durante o curso, consegui unir
+                      minhas paixões pela tecnologia e pelo desenvolvimento de
+                      software com a possibilidade de fazer desta atividade meu
+                      trabalho.
+                    </p>
                   </div>
-                  <div className="text-secondary-white text-xl">
-                    Tecnologo pela FATEC Guaratinguetá
-                  </div>
-                  <p>
-                    Formei-me em Análise e Desenvolvimento de Sistemas pela
-                    FATEC Guaratinguetá, o que marcou o início de minha carreira
-                    como programador. Durante o curso, consegui unir minhas
-                    paixões pela tecnologia e pelo desenvolvimento de software
-                    com a possibilidade de fazer desta atividade meu trabalho.
-                  </p>
                 </div>
-              </div>
-              <div>
-                <span>
-                  <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
-                </span>
-                <div className="px-5 flex flex-col gap-y-3">
-                  <div className=" flex items-center gap-x-2">
-                    <AiFillCalendar className="text-xl" /> 2022 - 2025
+                <div>
+                  <span>
+                    <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
+                  </span>
+                  <div className="px-5 flex flex-col gap-y-3">
+                    <div className=" flex items-center gap-x-2">
+                      <AiFillCalendar className="text-xl" /> 2022 - 2025
+                    </div>
+                    <div className="text-secondary-white text-xl">
+                      Tecnologo pela FATEC Guaratinguetá
+                    </div>
+                    <p>
+                      Formei-me em Análise e Desenvolvimento de Sistemas pela
+                      FATEC Guaratinguetá, o que marcou o início de minha
+                      carreira como programador. Durante o curso, consegui unir
+                      minhas paixões pela tecnologia e pelo desenvolvimento de
+                      software com a possibilidade de fazer desta atividade meu
+                      trabalho.
+                    </p>
                   </div>
-                  <div className="text-secondary-white text-xl">
-                    Tecnologo pela FATEC Guaratinguetá
-                  </div>
-                  <p>
-                    Formei-me em Análise e Desenvolvimento de Sistemas pela
-                    FATEC Guaratinguetá, o que marcou o início de minha carreira
-                    como programador. Durante o curso, consegui unir minhas
-                    paixões pela tecnologia e pelo desenvolvimento de software
-                    com a possibilidade de fazer desta atividade meu trabalho.
-                  </p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-span-1 text-white flex justify-center">a</div>
+          <div className="col-span-1 ml-20   text-white flex flex-col justify-center">
+            <TypingText
+              title="Experiências"
+              textStyles="text-center text-secondary-white font-bold mb-5 text-3xl"
+            />
+            <div className="bg-slate-800 p-5 rounded-xl shadow-lg shadow-slate-900">
+              <div className="w-full   border-l-2 flex flex-col border-orange-600 z-10">
+                <div>
+                  <span>
+                    <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
+                  </span>
+                  <div className="px-5 flex flex-col gap-y-3">
+                    <div className=" flex items-center gap-x-2">
+                      <AiFillCalendar className="text-xl" /> 2022 - 2025
+                    </div>
+                    <div className="text-secondary-white text-xl">
+                      Tecnologo pela FATEC Guaratinguetá
+                    </div>
+                    <p>
+                      Formei-me em Análise e Desenvolvimento de Sistemas pela
+                      FATEC Guaratinguetá, o que marcou o início de minha
+                      carreira como programador. Durante o curso, consegui unir
+                      minhas paixões pela tecnologia e pelo desenvolvimento de
+                      software com a possibilidade de fazer desta atividade meu
+                      trabalho.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <span>
+                    <AiTwotoneRightCircle className="relative right-[9px] top-5 text-orange-600 z-30" />
+                  </span>
+                  <div className="px-5 flex flex-col gap-y-3">
+                    <div className=" flex items-center gap-x-2">
+                      <AiFillCalendar className="text-xl" /> 2022 - 2025
+                    </div>
+                    <div className="text-secondary-white text-xl">
+                      Tecnologo pela FATEC Guaratinguetá
+                    </div>
+                    <p>
+                      Formei-me em Análise e Desenvolvimento de Sistemas pela
+                      FATEC Guaratinguetá, o que marcou o início de minha
+                      carreira como programador. Durante o curso, consegui unir
+                      minhas paixões pela tecnologia e pelo desenvolvimento de
+                      software com a possibilidade de fazer desta atividade meu
+                      trabalho.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </motion.div>
       </motion.div>
     </div>
