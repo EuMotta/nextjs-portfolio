50408e2b517e5020706a2f7c726891a061229541
EuMotta <hyperxmotta@hotmail.com>
2022-12-21 19:45:20 -0300

    gradient update

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Nav, Navbar } from '../components';
 import { About, About2, About3, Experience, Experience2, Explore, Feedback, GetStarted, Hero, Insights, Me, Objectives, Services, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className="bg-gradient-to-b from-slate-900 to-slate-700 overflow-hidden">
+  <div className="bg-gradient-to-b from-slate-900 to-[#262c31] overflow-hidden">
     <Navbar />
     <Nav />
     <Hero />
