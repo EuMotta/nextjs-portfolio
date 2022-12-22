c33a1088a9fb5f69c5a24f1dee4e5250f2a95084
EuMotta <hyperxmotta@hotmail.com>
2022-12-22 10:11:07 -0300

    Projects motion update

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { fadeIn, staggerContainer } from '../utils/motion';
 import { TypingText } from '../components';
 import styles from '../styles';
 import { projects } from '../constants';
@@ -12,9 +14,19 @@ const Projects = () => (
         title="Projetos Desenvolvidos"
         textStyles="text-center text-secondary-white font-bold p-10 mb-24 text-3xl"
       />
-      <div className="grid gap-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-        {projects.map(project => (
-          <div key={project.id} className="col-span-1">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className="grid gap-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1"
+      >
+        {projects.map((project) => (
+          <motion.div
+            variants={fadeIn(`${project.fadeIn}`, 'tween', 0, 1)}
+            key={project.id}
+            className="col-span-1"
+          >
             <div className="bg-slate-800 hover:-translate-y-3 transition-all flex flex-col justify-center rounded-xl shadow-lg shadow-slate-900 p-5">
               <div className="rounded-xl shadow-md  shadow-black overflow-hidden">
                 <Image
@@ -34,9 +46,9 @@ const Projects = () => (
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   </section>
 );
