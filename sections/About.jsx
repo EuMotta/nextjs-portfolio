636b4baf6e83ae3b4429a4dc2099d46049ce5ad7
EuMotta <hyperxmotta@hotmail.com>
2022-12-22 16:08:35 -0300

    about update

--- sections/About.jsx
+++ sections/About.jsx
@@ -14,37 +14,39 @@ const About = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
-      className="flex flex-col gap-y-28"
+      className="flex flex-col gap-y-24"
     >
       <TypingText
         title="Quem Sou eu?"
-        textStyles="text-center text-slate-600 dark:text-secondary-white font-bold p-10 mb-24 text-5xl"
+        textStyles="text-center text-slate-600 dark:text-secondary-white font-bold p-10 mb-12 text-5xl"
       />
-      {AboutSection.map((about) => (
-        <motion.div
-          id="About"
-          variants={staggerContainer}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.25 }}
-          key={about.id}
-        >
-          <motion.p
-            variants={fadeIn(`${about.fadeIn}`, 'tween', 0, 1)}
-            className={`${styles.xPaddings} mt-2 flex font-normal text-center text-secoundary-white`}
+      <div className="grid gap-y-20 grid-cols-2">
+        {AboutSection.map((about) => (
+          <motion.div
+            id="About"
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.25 }}
+            key={about.id}
           >
-            <span className="w-2/4 md:text-xl sm:text-md font-extrabold text-slate-900 dark:text-white">
-              {about.title}
-            </span>
-            <span className="w-3/4 md:text-md text-slate-700 dark:!text-secondary-white text-justify">
-              <span className="flex md:!text-xl sm:!text-md text-slate-900 dark:text-white mb-2">
-                <strong>{about.subtitle}</strong>
+            <motion.p
+              variants={fadeIn(`${about.fadeIn}`, 'tween', 0, 1)}
+              className={`${styles.xPaddings} mt-2 gap-x-2 flex font-normal text-center text-secoundary-white`}
+            >
+              <span className="w-2/4 md:text-xl grid sm:text-md font-extrabold text-slate-900 dark:text-white">
+                {about.title}
               </span>
-              {about.description}
-            </span>
-          </motion.p>
-        </motion.div>
-      ))}
+              <span className="w-3/4 md:text-md text-slate-700 dark:!text-secondary-white text-justify">
+                <span className="flex md:!text-md sm:!text-md text-slate-900 dark:text-white mb-2">
+                  <strong>{about.subtitle}</strong>
+                </span>
+                {about.description}
+              </span>
+            </motion.p>
+          </motion.div>
+        ))}
+      </div>
     </motion.div>
   </section>
 );
