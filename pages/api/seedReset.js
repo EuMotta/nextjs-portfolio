e80dbe4a71b066d81d6025dc2a9988c13dde8c92
EuMotta <hyperxmotta@hotmail.com>
2022-12-29 13:24:26 -0300

    seed reset system

---
+++ pages/api/seedReset.js
@@ -0,0 +1,15 @@
+import Feedback from '../../models/Feedback';
+import data from '../../utils/data';
+
+import db from '../../utils/db';
+
+const handler = async (req, res) => {
+  await db.connect();
+  await Feedback.deleteMany();
+  await Feedback.insertMany(data.feedbacks);
+  await db.disconnect();
+
+  res.send({ message: 'A SEED AGORA TA FUNFANDO!' });
+};
+
+export default handler;
