8178fad7ce8ec7e8182c059279c6132d10e1b85c
EuMotta <hyperxmotta@hotmail.com>
2023-03-08 23:29:59 -0300

    dashboard remove

--- components/Graphics/Dashboard.jsx
+++ components/Graphics/Dashboard.jsx
@@ -242,29 +242,29 @@ const Dashboard2 = () => (
 //     </ComposedChart>
 //   </ResponsiveContainer>
 // );
-const Dashboard4 = () => (
-  <ResponsiveContainer width="100%" height={300}>
-    <BarChart
-      width={500}
-      height={300}
-      data={rhData}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="mes" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="massa_salarial" fill="#4e3edf" />
-      <Brush />
-    </BarChart>
-  </ResponsiveContainer>
-);
+// const Dashboard4 = () => (
+//   <ResponsiveContainer width="100%" height={300}>
+//     <BarChart
+//       width={500}
+//       height={300}
+//       data={rhData}
+//       margin={{
+//         top: 5,
+//         right: 30,
+//         left: 20,
+//         bottom: 5,
+//       }}
+//     >
+//       <CartesianGrid strokeDasharray="3 3" />
+//       <XAxis dataKey="mes" />
+//       <YAxis />
+//       <Tooltip />
+//       <Legend />
+//       <Bar dataKey="massa_salarial" fill="#4e3edf" />
+//       <Brush />
+//     </BarChart>
+//   </ResponsiveContainer>
+// );
 const Dashboard5 = () => (
   <ResponsiveContainer width="100%" height={300}>
     <PieChart width={400} height={400}>
